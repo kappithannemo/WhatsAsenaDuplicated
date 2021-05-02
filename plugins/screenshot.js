a00e2c5e145993a323ea2f3b4a83658d64f6297a
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-05-02 15:18:12 +0530

    Update screenshot.js

--- plugins/screenshot.js
+++ plugins/screenshot.js
@@ -9,7 +9,7 @@ const fs = require('fs');
 const axios = require('axios');
 const request = require('request');
 const got = require("got");
-
+const Config = require('../config');
 const Language = require('../language');
 const Lang = Language.getString('webss');
 
@@ -32,4 +32,20 @@ Asena.addCommand({pattern: 'pss ?(.*)', fromMe: true, dontAddCommandList: true }
     await message.sendMessage(Buffer.from(webimage.data), MessageType.image, {mimetype: Mimetype.jpg})
 
 }));                 
+
+
+
+Asena.addCommand({pattern: 'spdf ?(.*)', fromMe: true, desc:"Converts a site into pdf" }, (async (message, match) => {
+
+    if (match[1] === '') return await message.sendMessage(Lang.LİNK);
+
+    var webimage = await axios.get(`
+https://api.html2pdf.app/v1/generate?url=${match[1]}&apiKey=${Config.PDF_API_KEY}`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer.from(webimage.data), MessageType.document, {mimetype: Mimetype.pdf})
+
+}));   
+
+
+
 
