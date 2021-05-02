107aa4f6032fd0026e6fd44ae1250c6c5ef4942f
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-05-02 15:30:16 +0530

    Update happymod.js

--- plugins/happymod.js
+++ plugins/happymod.js
@@ -2,7 +2,7 @@
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 WhatsAsenaDuplicated
-*/
+
 
 const Asena = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
