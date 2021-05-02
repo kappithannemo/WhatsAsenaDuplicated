9c4bedc6543c97014369998c9b6e665d9617eb3b
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-05-02 16:28:41 +0530

    Update bitly.js

--- plugins/bitly.js
+++ plugins/bitly.js
@@ -7,13 +7,13 @@ WhatsAsena - Yusuf Usta
 const Asena = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 const got = require('got');
-
+const Config = require('../config');
 const Language = require('../language');
 const Lang = Language.getString('weather');
 
 Asena.addCommand({pattern: 'bitly ?(.*)', fromMe: false, desc: Lang.BIT_DESC}, async (message, match) => {
 	if (match[1] === '') return await message.reply(Lang.NEED_LINK);
-	const url = `https://tobz-api.herokuapp.com/api/bitly?url=${match[1]}&apikey=BotWeA`;
+	const url = `https://tobz-api.herokuapp.com/api/bitly?url=${match[1]}&apikey=${Config.TOBZ_API_KEY}`;
 	try {
 		const response = await got(url);
 		const json = JSON.parse(response.body);
