3e715a85f689858b03cb66893a72232b389ba7f9
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-05-09 21:45:25 +0530

    Update exta.js

--- plugins/exta.js
+++ plugins/exta.js
@@ -1,3 +1,25 @@
+/* Copyright (C) 2021 Vai838.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+WhatsAsenaDuplicated
+*/
+
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+/*const got = require('got');
+const fs = require('fs');*/
+const axios = require('axios');
+
+const Language = require('../language');
+const Lang = Language.getString('weather');
+const { errorMessage, infoMessage } = require('../helpers');
+
+
+
+
+
+
+
 Asena.addCommand({ pattern: 'pic ?(.*)', fromMe: false,  dontAddCommandList: true}, async (message, match) => {
 
         
