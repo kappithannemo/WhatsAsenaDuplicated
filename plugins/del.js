9572f708e50d9ebc883241d5016e1f17bf3e309b
King isuwa <89562017+isuwabrooo@users.noreply.github.com>
2021-09-24 16:37:12 +0530

    Delete del.js

--- plugins/del.js
+++
@@ -1,9 +0,0 @@
-const Asena = require('../events');
-const {MessageType} = require('@adiwajshing/baileys');
-
-
-Asena.addCommand({pattern: 'del', fromMe: true, dontAddCommandList: true}, (async (message, match) => {
-
-  await message.reply_message.delete();
-
-}));
